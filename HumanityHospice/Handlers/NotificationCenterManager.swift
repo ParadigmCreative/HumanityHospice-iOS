562291cd78d3aa6802d2a7481f58e15630c42705
Luke Davis <russell.davis@okstate.edu>
2018-12-26 21:25:22 -0600

    Added Notification Center Manager

---
+++ HumanityHospice/Handlers/NotificationCenterManager.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationCenterManager.swift
+//  HumanityHospice
+//
+//  Created by App Center on 12/21/18.
+//  Copyright © 2018 Oklahoma State University. All rights reserved.
+//
+
+import Foundation
