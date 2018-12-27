3adb36480720bd2190773d9691f37d8962c77422
Luke Davis <russell.davis@okstate.edu>
2018-12-26 21:26:00 -0600

    Prepared for Notifications

--- HumanityHospice/AppDelegate.swift
+++ HumanityHospice/AppDelegate.swift
@@ -9,15 +9,16 @@
 import UIKit
 import Firebase
 import RealmSwift
+import NotificationCenter
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        NotificationDispatch.getNotificationSettings()
         FirebaseApp.configure()
         
         let version: UInt64 = 3
@@ -58,9 +59,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         Log.d("Did Enter Background")
-        
+        application.setMinimumBackgroundFetchInterval(1 * 60)
     }
 
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if AppSettings.currentAppUser is DatabaseHandler.Patient ||
+            AppSettings.currentAppUser is DatabaseHandler.Family {
+            registerDeviceForPatient()
+        } else if AppSettings.currentAppUser is DatabaseHandler.Reader {
+            registerDeviceForReader()
+        }
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        Log.d("Recieved notification")
+    }
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
@@ -79,6 +93,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     }
 
+    func registerDeviceForReader() {
+        let ref = DatabaseHandler.database.child("NotificationCenter").child(AppSettings.currentPatient!)
+        ref.child("Global").child("didUpdateJournal").observeSingleEvent(of: .value) { (snap) in
+            if let hasUpdated = snap.value as? Bool {
+                if hasUpdated {
+                    NotificationDispatch.notifyCurrentUser(name: AppSettings.currentPatientName!, type: NotificationDispatch.NotificationType.NewJournalPost)
+                }
+            }
+        }
+    }
+    
+    func registerDeviceForPatient() {
+        let ref = DatabaseHandler.database.child("NotificationCenter").child(AppSettings.currentPatient!)
+        ref.child("Patient").observeSingleEvent(of: .value) { (snap) in
+            if let items = snap.children.allObjects as? [DataSnapshot] {
+                
+                struct HHNotification {
+                    let url: DatabaseReference
+                    let name: String
+                    let action: String
+                }
+                
+                var notifications: [HHNotification] = []
+                
+                for item in items {
+                    if let data = item.value as? [String: Any] {
+                        let name = data["UserName"] as! String
+                        let action = data["Action"] as! String
+                        
+                        let notification = HHNotification(url: item.ref, name: name, action: action)
+                        
+                        switch notification.action {
+                        case "Commented on your Journal Post":
+                            NotificationDispatch.notifyCurrentUser(name: name, type: NotificationDispatch.NotificationType.NewJournalComment)
+                        case "Sent you encouragement":
+                            NotificationDispatch.notifyCurrentUser(name: name, type: NotificationDispatch.NotificationType.NewEncouragementPost)
+                        default:
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 
 }
 
--- HumanityHospice/Handlers/NotificationCenterManager.swift
+++ HumanityHospice/Handlers/NotificationCenterManager.swift
@@ -7,3 +7,114 @@
 //
 
 import Foundation
+import UserNotifications
+import FirebaseDatabase
+
+class NotificationDispatch {
+    static var center: UNUserNotificationCenter = .current()
+    static var options: UNAuthorizationOptions = [.alert, .badge, .sound]
+    
+    static public func startRequest() {
+        center.requestAuthorization(options: self.options) { (granted, error) in
+            guard error == nil else {
+                Log.e(error!.localizedDescription)
+                return
+            }
+            
+            if granted {
+                // Register for notifications
+            } else {
+                Log.e("You will not be notified of updates")
+            }
+        }
+    }
+    
+    static public func getNotificationSettings() {
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus != .authorized {
+                self.startRequest()
+            } else {
+                // Make sure user is registered
+            }
+        }
+    }
+    
+    static private func createRequest(title: String, body: String, sound: UNNotificationSound = UNNotificationSound.default()) -> UNMutableNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = sound
+        
+        return content
+    }
+    
+    static private func triggerRequest(title: String, body: String, sound: UNNotificationSound = .default()) {
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let content = createRequest(title: title, body: body, sound: sound)
+        let identifier = "UYLLocalNotification \(Date().timeIntervalSince1970)"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        center.add(request) { (error) in
+            guard error == nil else {
+                Log.e(error!.localizedDescription)
+                return
+            }
+        }
+    }
+    
+    static public func notifyCurrentUser(name: String, type: NotificationType) {
+        switch type {
+        case .NewJournalPost:
+            // Patient posted an update in their journal
+            triggerRequest(title: "New Journal Post", body: "\(name) has posted an update in their journal")
+        case .NewJournalComment:
+            // User commented on your journal post
+            triggerRequest(title: "New Comment on your Journal Post", body: "\(name) has commented on your journal post")
+        case.NewEncouragementPost:
+            // User Sent you encouragement
+            triggerRequest(title: "New Encouragement", body: "\(name) has sent you encouragement")
+        }
+    }
+    
+    static public func triggerNotification(for type: NotificationType) {
+        var ref = DatabaseHandler.database.child("NotificationCenter").child(AppSettings.currentPatient!)
+        if AppSettings.currentAppUser is DatabaseHandler.Patient || AppSettings.currentAppUser is DatabaseHandler.Family {
+            ref = ref.child("Patient")
+        } else {
+            ref = ref.child("Global")
+        }
+        
+        switch type {
+        case .NewEncouragementPost:
+            let data = ["UserName": "\(AppSettings.currentAppUser!.fullName())",
+                        "Action": "Sent you encouragement"]
+            ref.childByAutoId().setValue(data)
+            removeTriggerAfterSetTime(time: 65, type: type, ref: ref)
+        case .NewJournalComment:
+            let data = ["UserName": "\(AppSettings.currentAppUser!.fullName())",
+                        "Action": "Sent you encouragement"]
+            ref.childByAutoId().setValue(data)
+            removeTriggerAfterSetTime(time: 65, type: type, ref: ref)
+        case .NewJournalPost:
+            ref.child("didUpdateJournal").setValue("True")
+            removeTriggerAfterSetTime(time: 65, type: type, ref: ref)
+        }
+    }
+    
+    static private func removeTriggerAfterSetTime(time: TimeInterval, type: NotificationType, ref: DatabaseReference) {
+        Timer.scheduledTimer(withTimeInterval: time, repeats: false) { (timer) in
+            switch type {
+            case .NewJournalPost:
+                ref.setValue(false)
+            case .NewEncouragementPost, .NewJournalComment:
+                ref.setValue(nil)
+            }
+        }
+    }
+    
+    enum NotificationType {
+        case NewJournalPost
+        case NewJournalComment
+        case NewEncouragementPost
+    }
+    
+}
